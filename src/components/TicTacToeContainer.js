c585286cd3d963f6cba738362e7cd178c767ac17
Lalita Phalak <lalita@edcast.com>
2017-10-10 15:47:03 +0530

    Initial setup

---
+++ src/components/TicTacToeContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TicTacToeContainer extends Component {
+  render() {
+    return (
+      <div className="container">
+        I am in TicTac
+      </div>
+    );
+  }
+}
+
+export default TicTacToeContainer;
